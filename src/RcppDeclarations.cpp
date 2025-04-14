0fb7f73d6fee179ebe3c586c7fa248724f3691f3
Manos Papadakis <papadakm95@gmail.com>
2025-04-14 17:39:50 +0300

    add

--- src/RcppDeclarations.cpp
+++ src/RcppDeclarations.cpp
@@ -4,26 +4,41 @@
 #include "Str.h"
 
 //[[Rcpp::export]]
-NumericVector generate_minstd_rand_sequence(std::size_t length) {  // 2. minstd_rand
+NumericVector Rminstd_rand(std::size_t length) {  // 2. minstd_rand
     return Random::Rminstd_rand<NumericVector>(length);
 }
 
 //[[Rcpp::export]]
-NumericVector generate_mt19937_sequence(std::size_t length) {  // 3. mt19937
+NumericVector Rmt19937(std::size_t length) {  // 3. mt19937
     return Random::Rmt19937<NumericVector>(length);
 }
 
 //[[Rcpp::export]]
-NumericVector generate_knuth_b_sequence(std::size_t length) {  // 9. knuth_b
+NumericVector Rknuth_b(std::size_t length) {  // 9. knuth_b
     return Random::Rknuth_b<NumericVector>(length);
 }
 
-//[[Rcpp::export]]
-NumericVector sample(NumericVector& input, std::size_t size, bool replace = false){
-    return Random::sample<NumericVector>(input,size,replace);
+//[[Rcpp::export(name = "Rminstd_rand.int")]]
+IntegerVector Rminstd_rand_int(std::size_t length) {  // 2. minstd_rand
+    return Random::Rminstd_rand<IntegerVector>(length);
+}
+
+//[[Rcpp::export(name = "Rmt19937.int")]]
+IntegerVector Rmt19937_int(std::size_t length) {  // 3. mt19937
+    return Random::Rmt19937<IntegerVector>(length);
+}
+
+//[[Rcpp::export(name = "Rknuth_b.int")]]
+IntegerVector Rknuth_b_int(std::size_t length) {  // 9. knuth_b
+    return Random::Rknuth_b<IntegerVector>(length);
 }
 
 //[[Rcpp::export]]
+NumericVector Sample(NumericVector& input, std::size_t size, bool replace = false){
+    return Random::sample<NumericVector>(input,size,replace);
+}
+
+//[[Rcpp::export(name = "Sample.int")]]
 IntegerVector sample_int(IntegerVector& input, std::size_t size, bool replace = false){
     return Random::sample<IntegerVector>(input,size,replace);
 }
@@ -43,7 +58,7 @@ std::string replaceSubstring(std::string s, std::string oldSubstr, std::string n
     return Str::replaceSubstring(s, oldSubstr, newSubstr);
 }
 
-//[[Rcpp::export]]
+//[[Rcpp::export(name = "toTable.char")]]
 std::map<std::string, double> toTable_char(std::string x){
     return Str::toTable(x);
 }
